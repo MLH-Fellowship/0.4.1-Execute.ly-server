d4d5d7d9eea780689113fea52a9971678304e2f0
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-06-05 02:23:02 +0530

    fix google credentials issue

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@
  * This file generates a server instance
  */
 import dotenv from "dotenv";
-
+import path from "path";
 dotenv.config();
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = path.join(
+  __dirname,
+  "..",
+  process.env.GOOGLE_CLOUD_JSON
+);
+
 const PORT = process.env.SERVER_PORT;
 import app from "./app";
 
